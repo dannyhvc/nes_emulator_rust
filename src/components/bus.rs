f4d4c1c68712bcc6ad2e070f4a05ebe2d8c01eb0
Daniel Herrera <26307270+dannyhvc@users.noreply.github.com>
2023-11-02 00:34:41 -0400

    working on parsing the opcodes

--- src/components/bus.rs
+++ src/components/bus.rs
@@ -44,10 +44,34 @@ impl Bus {
 
     #[cfg(feature = "debug")]
     pub fn load_instruction_mem(&mut self, data: Box<[Box<[u16]>]>) {
-        const PHYSICAL_ADDR: usize = 0;
-        const OPCODE_ADDR: usize = 1;
-        for cin in data.iter() {
-            self.write(cin[PHYSICAL_ADDR], cin[OPCODE_ADDR] as u8);
+        // we subtract 1 because we want to exclude the instruction location by default
+
+        // TODO: keep this for now so we can test dan;s brain
+        use super::types::AddrModeMneumonic;
+        use crate::components::types::OpcodeMneumonic;
+
+        for ins in data.iter() {
+            let current_instruction: &[u16] = ins.iter().as_slice();
+            assert!(current_instruction.len() > 1, "instruction invalid");
+
+            let ins_addr: u16 = current_instruction[0];
+            let opcode: u8 = current_instruction[1] as u8;
+            match current_instruction.len() - 1 {
+                // 0 byte operand
+                1 => {}
+
+                // 1 byte operand
+                2 => {
+                    let operand: u8 = current_instruction[2] as u8;
+                }
+
+                // 2 byte operand
+                3 => {
+                    let high_byte_operand: u8 = current_instruction[2] as u8;
+                    let low_byte_operand: u8 = current_instruction[3] as u8;
+                }
+                _ => panic!("Instruction operand length can be either 0,1, or 2 bytes"),
+            };
         }
     }
 }
