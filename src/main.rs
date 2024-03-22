eb7e2c8baa3c5826f72252b741c24be16bcda8c6
Daniel Herrera <26307270+dannyhvc@users.noreply.github.com>
2024-03-22 00:49:50 -0400

    updated example for debug tui

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,36 @@ mod tests;
 
 mod macros;
 
-use nes_debug as nesd;
+use nes_debug::{self as nesd, ratatui::widgets as w};
+use nesd::Stylize;
 
-fn ui(frame: &mut nesd::Frame) {}
+fn ui(frame: &mut nesd::Frame) {
+    let (area, layout) = nesd::calc_layout(frame.size());
+
+    // made a random paragraph
+    let p = w::Paragraph::new("Dan made a paragraph")
+        .alignment(nesd::Alignment::Center);
+
+    // made a random table
+    let mut table_state = w::TableState::default();
+    let rows = [
+        w::Row::new(vec!["Row11", "Row12", "Row13"]),
+        w::Row::new(vec!["Row21", "Row22", "Row23"]),
+        w::Row::new(vec!["Row31", "Row32", "Row33"]),
+    ];
+    let widths = [
+        nesd::Constraint::Length(5),
+        nesd::Constraint::Length(5),
+        nesd::Constraint::Length(10),
+    ];
+    let table = w::Table::new(rows, widths)
+        .block(w::Block::default().title("Table"))
+        .highlight_style(
+            nesd::Style::new().add_modifier(nesd::Modifier::REVERSED),
+        )
+        .highlight_symbol(">>");
+    frame.render_stateful_widget(table, area, &mut table_state);
+}
 
 fn main() -> nesd::Result<()> {
     let mut terminal: nesd::Terminal = nesd::setup_terminal()?;
