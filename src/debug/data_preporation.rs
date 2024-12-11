286d3c137b9c41b9404b597cf7d8d19e969861e1
dannyhvc <danherrerav@gmail.com>
2024-12-11 15:48:39 -0500

    feat: HexView for debugging and start/stop added for RRVD
    Signed-off-by: dannyhvc <danherrerav@gmail.com>

--- src/debug/data_preporation.rs
+++ src/debug/data_preporation.rs
@@ -1,10 +1,38 @@
-use super::traits::RawRamViewDimensions;
+use super::traits::{HexView, RawRamViewDimensions};
 use super::types::DebuggerState;
 
-impl RawRamViewDimensions<0xF, 0xF0> for DebuggerState {
+impl HexView for DebuggerState {
+    fn display(&self, words_list: Vec<Vec<&u8>>) -> String {
+        use std::io::Write;
+        let mut fmt_buff = Vec::new();
+
+        words_list.iter().for_each(|word| {
+            word.iter().for_each(|byte| {
+                let hi_lo: String =
+                    format!("{:X}{:X} ", *byte >> 4, *byte & 0x0F);
+                write!(&mut fmt_buff, "{}", hi_lo);
+            });
+            writeln!(&mut fmt_buff, "");
+        });
+
+        String::from_utf8(fmt_buff)
+            .expect("Couldn't format nibbles into HexView")
+    }
+}
+
+impl RawRamViewDimensions<0xF, 0xF0, 0x0000, 0x0800> for DebuggerState {
     fn first_n_words(&self) -> Vec<Vec<&u8>> {
         let binding = self.bus.ram();
-        let words: Vec<_> = binding.iter().take(Self::HEIGHT).collect();
+        assert!(
+            Self::START + Self::HEIGHT < binding.len(),
+            "Size of the binding is smaller than the size of the START stride"
+        );
+
+        let words: Vec<_> = binding
+            .iter()
+            .skip(Self::START)
+            .take(Self::HEIGHT)
+            .collect();
         let words: Vec<_> = words
             .chunks(Self::WIDTH)
             .map(|chunck| chunck.to_vec())
@@ -14,8 +42,12 @@ impl RawRamViewDimensions<0xF, 0xF0> for DebuggerState {
 
     fn last_n_words(&self) -> Vec<Vec<&u8>> {
         let binding = self.bus.ram();
-        let words: Vec<_> = binding.iter().rev().take(Self::HEIGHT).collect();
+        assert!(
+            Self::START + Self::HEIGHT < binding.len(),
+            "Size of the binding is smaller than the size of the END stride"
+        );
 
+        let words: Vec<_> = binding.iter().rev().take(Self::HEIGHT).collect();
         let words: Vec<Vec<_>> = words
             .chunks(Self::WIDTH)
             .map(|chunck| chunck.to_vec())
@@ -26,7 +58,7 @@ impl RawRamViewDimensions<0xF, 0xF0> for DebuggerState {
 
 #[cfg(test)]
 mod tests {
-    use crate::debug::traits::RawRamViewDimensions;
+    use crate::debug::traits::{HexView, RawRamViewDimensions};
 
     use super::super::types::*;
     use rstest::{fixture, rstest};
@@ -50,5 +82,13 @@ mod tests {
         // Add assertions to verify the behavior.
         assert_eq!(result.len(), DebuggerState::HEIGHT / DebuggerState::WIDTH);
         assert_eq!(result[0].len(), DebuggerState::WIDTH);
+    }
+
+    #[rstest]
+    fn test_HexView_to_string(state: DebuggerState) {
+        let display = state.display(state.first_n_words());
+        println!("{display}");
+        let display = state.display(state.last_n_words());
+        println!("{display}");
     }
 }
--- src/debug/traits.rs
+++ src/debug/traits.rs
@@ -1,8 +1,20 @@
 /// Simple
-pub trait RawRamViewDimensions<const WIDTH: usize, const HEIGHT: usize> {
+pub trait RawRamViewDimensions<
+    const WIDTH: usize,
+    const HEIGHT: usize,
+    const START: usize,
+    const END: usize,
+>
+{
     const WIDTH: usize = WIDTH;
     const HEIGHT: usize = HEIGHT;
+    const START: usize = START;
+    const END: usize = END;
 
     fn first_n_words(&self) -> Vec<Vec<&u8>>;
     fn last_n_words(&self) -> Vec<Vec<&u8>>;
+}
+
+pub trait HexView {
+    fn display(&self, words_list: Vec<Vec<&u8>>) -> String;
 }
